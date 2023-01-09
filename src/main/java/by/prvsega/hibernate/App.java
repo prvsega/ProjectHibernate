02e9a7b5632108a8a0675caf22fd0cf72242e4a2
Pavel Rakovich <prvsega@gmail.com>
2023-01-09 18:11:31 +0300

    Hibernate ManyToMany (actor_movie)

--- src/main/java/by/prvsega/hibernate/App.java
+++ src/main/java/by/prvsega/hibernate/App.java
@@ -1,13 +1,12 @@
 package by.prvsega.hibernate;
 
-import by.prvsega.model.Item;
-import by.prvsega.model.Passport;
-import by.prvsega.model.Person;
+import by.prvsega.model.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -16,24 +15,29 @@ import java.util.List;
  */
 public class App {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Movie.class)
+                .addAnnotatedClass(Actor.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
 
-        Session session = sessionFactory.getCurrentSession();
 
-
-        try {
+        try (sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-           Person person = session.get(Person.class, 12);
+            Actor actor = session.get(Actor.class, 2);
 
-           session.delete(person);
+            System.out.println(actor.getMovies());
+
+            Movie movieToRemove = actor.getMovies().get(0);
+
+            actor.getMovies().remove(movieToRemove);
+
+            movieToRemove.getActors().remove(actor);
+
+            session.update(movieToRemove);
 
             session.getTransaction().commit();
-        } finally {
-            sessionFactory.close();
         }
 
 
